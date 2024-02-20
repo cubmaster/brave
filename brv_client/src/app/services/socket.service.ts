f1f1f97ec618c597a0574cdb6317efe9f08b3d4a
Cubmaster <tad@tadmcclellan.com>
2024-02-19 20:35:36 -0600

    working with autogen

---
+++ brv_client/src/app/services/socket.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private socket: Socket;
+  private url = 'ws://localhost:5000'; // Your Socket.IO server URL
+
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+  // Method to emit events
+  emitEvent(event: string, data: any): void {
+    this.socket.emit(event, data);
+  }
+  joinRoom(data: {username: string, room: string}) {
+    this.socket.emit('join', data);
+  }
+
+  // Method to listen to events
+  listenEvent(event: string, callback: (data: any) => void): void {
+    this.socket.on(event, callback);
+  }
+
+  // Disconnect the socket
+  disconnect(): void {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
